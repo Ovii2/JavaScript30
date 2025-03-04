b87cc4de25a59580698afbc150e95e477f863b94
Ovidijus <ovidijus.e@gmail.com>
2025-03-04 09:55:32 +0200

    Key sequence

---
+++ 12.Day-12-key-sequence/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const pressed = [];
+const secretCode = 'test';
+
+window.addEventListener('keyup', (e) => {
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode)) {
+    console.log('You did it!');
+  }
+});
