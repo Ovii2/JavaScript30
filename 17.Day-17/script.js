41077849fa60ee8a66f262f07229043dfa520573
Ovidijus <ovidijus.e@gmail.com>
2025-03-07 14:50:56 +0200

    Sort without prefix

---
+++ 17.Day-17/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog',
+];
+
+const bandsWithoutPrefix = (bandName) => bandName.replace(/^(a |the |an )/i, '').trim();
+
+const sortedBands = bands.sort((a, b) => (bandsWithoutPrefix(a) > bandsWithoutPrefix(b) ? 1 : -1));
+
+document.querySelector('#bands').innerHTML = sortedBands.map((band) => `<li>${band}</li>`).join('');
